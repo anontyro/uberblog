af48ffe75d02c18b11240ca88515fa062159c0fd
anontyro <alexwilkinson@gmail.com>
2017-01-10 16:57:50 +0800

    pagation
    Working on the pagation and displaying of the blogList correctly, will
    need to limited extras show on following pages

--- pages/bloglist.php
+++ pages/bloglist.php
@@ -24,6 +24,17 @@ include("../header.php");
 		$numberOfPages = ceil(($output / $postsPerPage));
 	}
 	$numberOfPages = ceil(($output / $postsPerPage));
+	if($numberOfPages > 1){
+	?>
+		<button class="btn btn-default"><</button>
+		<?php 
+			for($x = 0; $x < $numberOfPages; $x++){
+				echo '<a href="/uberblog/pages/bloglist.php?page='.$x.'" class="btn btn-primary" role="button"> '.($x+1).' </a> ';
+			}
+		 ?>
+		 <a href="/uberblog/pages/bloglist.php?page=<?php echo ($_GET['page']+1); ?>"  class="btn btn-primary" role="button"> > </a>
+	<?php
+	}
 	echo "number of pages = $numberOfPages";
 	?>
 </div>
